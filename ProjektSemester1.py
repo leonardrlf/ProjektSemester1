6896315971e1bc20c7a813f1b79cfd8aac7a2071
NoahKlug <noahklug5@gmail.com>
2024-03-07 13:25:13 +0100

    ausgabe des wortes

--- ProjektSemester1.py
+++ ProjektSemester1.py
@@ -16,8 +16,15 @@ random_Wort():
 
 
 def
-wort_Darstellung(wort,
-erratene_Buchstaben):
+wort_Darstellung(wort,erratene_Buchstaben):
+display = ""                                    ##Erstellen von Ausgabevariable für das Wort oder erratene Buchstaben 
+for buchstabe in wort:                          ## Wenn ein Buchstabe in einem Wort erraten wurde wird er zu der Displayausgabe hinzugefügt
+ if buchstabe in erratene_buchstabe:
+  display += buchstabe
+else:                                           ## Für alle noch nicht erratenen Buchstaben im Wort schreibe "_"
+ display += "_"
+
+return display
 
 
 
