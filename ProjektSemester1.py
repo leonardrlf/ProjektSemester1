4ed742ea51c50fdd3345b1fe40815dcc08bed0f7
leonardrlf <leonard.rolf@mercedes-benz.com>
2024-03-07 15:52:24 +0100

    Hier die Funktion des Random Wortes und der Visualisierung des Galgenmannes

--- ProjektSemester1.py
+++ ProjektSemester1.py
@@ -2,16 +2,19 @@
 ########## Autoren: Leonard Rolf, Levin Köppe, Younes Bareche, Noah Klug, Simon Kieslich####
 ###### Erstellt vom 05.03.2024 bis 21.03.2024 ###############
 
+# import modul random
 import random   # import modul "random", so we have acces to random generator
 
+# defining function random_Wort
 def random_Wort():  # function, which brings back a random word from our collection
     woerter = ["karlsruhe","darmstadt","bremerhafen","hamburg","berlin","köln","kiel","nuernberg","chemnitz","cottbus","saarbruecken","trier","neubrandenburg"] # our word collection: citys of Germany
     return random.choice(woerter) # end of function, return to main with random word
 
 def wort_Darstellung(wort, erratene_Buchstaben):
-    
 
 
+
+# defining function galgenmann
 def galgenmann(versuche): # function, which visualizes the amount of trys by creating the hangman step by step
     ettapen = [           # variable depends on trys left: variable n shows hangman graphic no. n
                           # figure 1 to 7
