7e7dcf39cee2c00374b6b44e36897e5bfcb51511
leonardrlf <leonard.rolf@mercedes-benz.com>
2024-03-07 15:48:33 +0100

    Hier die FUnktion des Random Wortes und der Visualisierung des Galgenmannes

--- ProjektSemester1.py
+++ ProjektSemester1.py
@@ -2,43 +2,93 @@
 ########## Autoren: Leonard Rolf, Levin Köppe, Younes Bareche, Noah Klug, Simon Kieslich####
 ###### Erstellt vom 05.03.2024 bis 21.03.2024 ###############
 
-<<<<<<< HEAD
-import
- random
+import random   # import modul "random", so we have acces to random generator
+
+def random_Wort():  # function, which brings back a random word from our collection
+    woerter = ["karlsruhe","darmstadt","bremerhafen","hamburg","berlin","köln","kiel","nuernberg","chemnitz","cottbus","saarbruecken","trier","neubrandenburg"] # our word collection: citys of Germany
+    return random.choice(woerter) # end of function, return to main with random word
+
+def wort_Darstellung(wort, erratene_Buchstaben):
+    
 
 
-def
-random_Wort():
+def galgenmann(versuche): # function, which visualizes the amount of trys by creating the hangman step by step
+    ettapen = [           # variable depends on trys left: variable n shows hangman graphic no. n
+                          # figure 1 to 7
+        """
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+       /|   |
+            |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+       /|\  |
+            |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+       /|\  |
+       /    |
+            |
+        =========
+        """,
+       
+        """
+        +---+
+        |   |
+        O   |
+       /|\  |
+       / \  |
+            |
+        =========
+        """
+        ]
+    print(ettapen[versuche]) # the hangman is printed, depending on the amount of trys
+
+def versuche_bis_verloren():
 
 
 
-
-
-
-def
-wort_Darstellung(wort,
-erratene_Buchstaben):
-
-
-
-
-
-
-def
-galgenmann(versuche):
-
-
-
-
-
-
-def
-versuche_bis_verloren():
-
-
-
-
-
-
-def
-galgenmännchen():
+def galgenmännchen():
