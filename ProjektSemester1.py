91043d7dcae53928f22ecadebe301f2973963867
leonardrlf <leonard.rolf@mercedes-benz.com>
2024-03-07 12:44:54 +0100

    so

--- ProjektSemester1.py
+++ ProjektSemester1.py
@@ -2,6 +2,42 @@
 ########## Autoren: Leonard Rolf, Levin Köppe, Younes Bareche, Noah Klug, Simon Kieslich####
 ###### Erstellt vom 05.03.2024 bis 21.03.2024 ###############
 
-bereit =True
-input
-test
+import
+ random
+
+
+def
+random_Wort():
+
+
+
+
+
+
+def
+wort_Darstellung(wort,
+erratene_Buchstaben):
+
+
+
+
+
+
+def
+galgenmann(versuche):
+
+
+
+
+
+
+def
+versuche_bis_verloren():
+
+
+
+
+
+
+def
+galgenmännchen():
